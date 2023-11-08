991d19edbac5c309a15207a0e3f97f8cef9ce068
Vinícius Oliveira Fernandes <99264567+Vfernandes1@users.noreply.github.com>
2023-11-07 22:23:12 -0300

    Update lamba_function.py

--- src/lamba_function.py
+++ src/lamba_function.py
@@ -9,12 +9,8 @@ def lambda_handler(event, context):
     username = event.get('queryStringParameters', {}).get('username')
     password = event.get('queryStringParameters', {}).get('password')
 
-    # Check if both username and password are provided
     if username and password:
-        # Check if the provided username and password are valid
         if username == USUARIO_ESPERADO and password == SENHA_ESPERADA:
-            # Authentication successful, allow access to the POST route
-            # TODO: Implemente sua lógica para a rota POST aqui
     
             return {
                 'statusCode': 200,
